be3ef453fb4d87251ed020eb38efef2cd899d2c1
Ganesh Bellale <ganeshbellale45@gmail.com>
2025-05-04 12:54:16 +0530

    Initial commit

---
+++ src/main/java/com/ro/GlobalExceptionHandlingPracticeAppApplication.java
@@ -0,0 +1,14 @@
+package com.ro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalExceptionHandlingPracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalExceptionHandlingPracticeAppApplication.class, args);
+
+	}
+
+}
